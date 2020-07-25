e4fc71a96bce199538f349025ce4bc0af93f4663
Sheng Chang <shenchan@microsoft.com>
2020-07-25 17:25:20 +0800

    move to vscode

---
+++ vscode/K01-BasicLinq.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace CSharpKatas
+{
+    public class Calculator
+    {
+        public int SumNumbers(int start, int count)
+        {
+            // todo #1: Remove the Ignore attribute and get all the tests passing
+            // todo #2: Refactor so there are no loops or if statements (this is the functional approach and is idiomatic for C#)
+            // todo #3: Refactor to use the Aggregate() LINQ method (see 101 Linq Samples: http://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b)
+            var ret = Enumerable.Range(start, count).Sum();
+            return ret;
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void SumNumbers_StartAt100SumNext1_TotalIs100()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(100, kata.SumNumbers(100, 1));
+        }
+
+        [Test]
+        public void SumNumbers_StartAt100SumNext2_TotalIs201()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(201, kata.SumNumbers(100, 2));
+        }
+        [Test]
+        public void SumNumbers_StartAt100SumNext0_TotalIs0()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(0, kata.SumNumbers(100, 0));
+        }
+
+        [Test]
+        public void SumNumbers_StartAt0SumNext0_TotalIs0()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(0, kata.SumNumbers(0, 0));
+        }
+
+        [Test]
+        public void SumNumbers_StartAt0SumNext3_TotalIs3()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(3, kata.SumNumbers(0, 3));
+        }
+
+        [Test]
+        public void SumNumbers_StartAt0SumNext100_TotalIs5050()
+        {
+            var kata = new Calculator();
+            Assert.AreEqual(5050, kata.SumNumbers(1, 100));
+        }
+    }
+}
